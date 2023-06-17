f8c783500484c8164374ec6fb95bb86b067c12b9
mithulix <mohaiminul.devs@gmail.com>
2023-06-17 09:24:04 +0600

    genericInterface

--- src/mission_1/module_3/genericInterface.ts
+++ src/mission_1/module_3/genericInterface.ts
@@ -1,3 +1,23 @@
-interface GenericInterface<T>{name:string, husband: T};
+interface GenericInterface<T, U = null>{name:string, husband:T, wife?: U};
 
-const interface1: GenericInterface<boolean> = {name: "persiana", husband: true}
+const interfaceVariable1: GenericInterface<boolean, string> = {name: "persiana", husband: true, wife: "sundori"};
+const interfaceVariable2: GenericInterface<string> = { name: "persiana", husband: "peter"}
+
+
+interface husbandInterface {name: string; salary: number}
+const interfaceVariable3: GenericInterface<husbandInterface> = { 
+        name: "persiana", 
+        husband: {
+            name: "peter",
+            salary: 555555
+        }
+    }
+
+interface husbandWifeInterface {name:string, age:number };
+const interfaceVariable4: GenericInterface<husbandWifeInterface, husbandWifeInterface> = {
+    name: "pixer",
+    husband: {
+        name: "pixy",
+        age: 100
+    }
+}
